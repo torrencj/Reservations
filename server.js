782900a0f5cc242fb75b8f483e555d2ae9a8c8a8
John <16932417+torrencj@users.noreply.github.com>
2017-09-14 14:23:04 -0500

    added file serve routes

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
 
 
 // Logic
